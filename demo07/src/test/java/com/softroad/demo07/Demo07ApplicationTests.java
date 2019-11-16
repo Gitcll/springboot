8c92db5cbd49b3f78a3df83fd2af9173efd2e3d8
Gitcll <302701569@qq.com>
2019-11-16 15:29:22 +0800

    'init'

---
+++ demo07/src/test/java/com/softroad/demo07/Demo07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softroad.demo07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
